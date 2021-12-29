66974053c3e3a3c1f027d2d76a3ab3bd13f6c364
iamgodot <xugodot@gmail.com>
2021-12-29 17:10:50 +0800

    Mark convenient functions

--- echo/echo-client.py
+++ echo/echo-client.py
@@ -6,8 +6,8 @@ ADDR = "127.0.0.1"
 PORT = 65432
 
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-with socket.create_connection((ADDR, PORT)) as s:
-    # s.connect((ADDR, PORT))  # blocking calls, as well as send and recv
+# s.connect((ADDR, PORT))  # blocking calls, as well as send and recv
+with socket.create_connection((ADDR, PORT)) as s:  # Convenient function
     s.sendall(b"Hello world!")
     # 这里的 recv 不一定会返回全部的数据，如果只返回了部分
     # 那么 socket close 之后，就会造成数据丢失（缓冲区），
--- echo/echo-server.py
+++ echo/echo-server.py
@@ -5,9 +5,10 @@ import socket
 ADDR = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((ADDR, PORT))
-    s.listen()
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+# s.bind((ADDR, PORT))
+# s.listen()
+with socket.create_server((ADDR, PORT)) as s:  # Convenient function
     conn, addr = s.accept()  # accept blocks
     print("Connected by ", addr)
     while True:
